599f4100eedd619796b637e1f346c498d37b1a61
François Saint-Jacques <fsaintjacques@gmail.com>
2021-08-19 12:56:27 -0400

    Add cli package

---
+++ cli/profile.go
@@ -0,0 +1,50 @@
+package cli
+
+import (
+	"github.com/pkg/profile"
+)
+
+type (
+	// Profiling is a struct that can be embedded in any kong cli to enable
+	// easy and convenient profiling for performance analysis. The flag is
+	// hidden from the help message such that we can use it in public cli.
+	//
+	// The supported values are:
+	//   - "cpu":    Enables CPU profiling.
+	//   - "memory": Enables heap memory profiling.
+	//   - "block":  Enables block (contention) profiling.
+	//   - "mutex":  Enables mutex profiling.
+	//   - "trace":  Enables trace profiling.
+	//
+	// The profiling file path will be shown to stderr and can be opened with
+	// `go tool pprof $file` or `go tool pprof -http localhost:8080 $file`
+	//
+	// In order to enable profiling, one should use the command like this:
+	// ```
+	// stopProfiling := cli.Profiling.Start()
+	// defer stopProfiling()
+	// ```
+	Profiling struct {
+		Profiling string `opt:"" hidden:"true" default:""`
+	}
+)
+
+// Start starts the profiling operation. It returns a function that needs to be
+// called when the profiling should stop.
+func (p *Profiling) Start() func() {
+	path := profile.ProfilePath(".")
+	switch p.Profiling {
+	case "cpu":
+		return profile.Start(path, profile.CPUProfile).Stop
+	case "memory":
+		return profile.Start(path, profile.MemProfile).Stop
+	case "block":
+		return profile.Start(path, profile.BlockProfile).Stop
+	case "mutex":
+		return profile.Start(path, profile.MutexProfile).Stop
+	case "trace":
+		return profile.Start(path, profile.TraceProfile).Stop
+	default:
+		return func() {}
+	}
+}
--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/optable/optable-pkglib
 
 go 1.16
 
-require github.com/stretchr/testify v1.7.0
+require (
+	github.com/pkg/profile v1.6.0
+	github.com/rs/zerolog v1.23.0
+	github.com/stretchr/testify v1.7.0
+)
