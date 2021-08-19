123ab1cc57639f0df4e13e116f46ade1b5dd074f
François Saint-Jacques <fsaintjacques@gmail.com>
2021-08-19 12:53:56 -0400

    Add unit package

---
+++ unit/bytes.go
@@ -0,0 +1,18 @@
+package unit
+
+const (
+	Byte = 1
+
+	// The binary (IEC) prefix are powers of 1024.
+	Kibibyte = Byte * 1024
+	Mebibyte = Kibibyte * 1024
+	Gibibyte = Mebibyte * 1024
+	Tebibyte = Gibibyte * 1024
+	Pebibyte = Tebibyte * 1024
+
+	KiB = Kibibyte
+	MiB = Mebibyte
+	GiB = Gibibyte
+	TiB = Tebibyte
+	PiB = Pebibyte
+)
---
+++ unit/bytes_test.go
@@ -0,0 +1,23 @@
+package unit
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestByteUnits(t *testing.T) {
+	assert.Equal(t, 1, Byte)
+
+	assert.Equal(t, 1024, Kibibyte)
+	assert.Equal(t, 1024*1024, Mebibyte)
+	assert.Equal(t, 1024*1024*1024, Gibibyte)
+	assert.Equal(t, 1024*1024*1024*1024, Tebibyte)
+	assert.Equal(t, 1024*1024*1024*1024*1024, Pebibyte)
+
+	assert.Equal(t, KiB, Kibibyte)
+	assert.Equal(t, MiB, Mebibyte)
+	assert.Equal(t, GiB, Gibibyte)
+	assert.Equal(t, TiB, Tebibyte)
+	assert.Equal(t, PiB, Pebibyte)
+}
